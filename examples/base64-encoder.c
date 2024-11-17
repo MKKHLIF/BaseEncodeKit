7e70a0167b7f25727254bf5dad2bc7190c303be0
Mayssam-Khlif <khlif.mk@proton.me>
2024-11-17 21:34:55 +0100

    base64 encoding example (#1)

--- examples/base64-encoder.c
+++ examples/base64-encoder.c
@@ -1,6 +1,13 @@
+#include <stdio.h>
 #include "base64.h"
 
-int main (int argc, char *argv[]) {
 
+int main(int argc, char *argv[]) {
+    const char *input = "f";
+
+    char *encoded = base64_encode(input, 1);
+    printf("Encoded: %s\n", encoded);
+
+    printf("\n");
     return 0;
 }
