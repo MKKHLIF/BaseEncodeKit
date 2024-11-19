9e6e5034f4edb71e5e0b798f8c8655b9b0b7a88c
Mayssam-Khlif <khlif.mk@proton.me>
2024-11-19 12:44:32 +0100

    Initial base64 enc test (#1)

--- tests/base64.c
+++ tests/base64.c
@@ -1,1 +1,25 @@
-#include <unity.h>
+#include "unity.h"
+#include "base64.h"
+
+#include <stdlib.h>
+
+void setUp(void) {
+}
+
+void tearDown(void) {
+}
+
+
+void test_base64_encoding(void) {
+    char *encoded = base64_encode("Hello, World!", 13);
+    TEST_ASSERT_EQUAL_STRING("SGVsbG8sIFdvcmxkIQ==", encoded);
+    free(encoded);
+}
+
+int main(void) {
+    UNITY_BEGIN();
+
+    RUN_TEST(test_base64_encoding);
+
+    return UNITY_END();
+}
